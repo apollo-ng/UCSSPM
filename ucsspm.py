93dbc64e72e673d99d64b473918cb2f8ac1a718b
chrono <chrono@open-resource.org>
2014-11-11 17:34:02 +0000

    Refactored output

--- ucsspm.py
+++ ucsspm.py
@@ -30,11 +30,34 @@
 
 import sys, argparse, math, time, calendar
 
-################################################################################
-##  Setup  #####################################################################
-################################################################################
+def output(opt,res):
 
-def setup(arg):
+    print "--------------------------------------------------------------------"
+    print "%d.%d.%d | %d | %f | " % (opt.day, opt.month, opt.year, res['DoY'], res['ToD'] )
+    print "--------------------------------------------------------------------"
+    print "Solar Constant                               : %s" % opt.sc
+    print "Atmospheric turbidity coefficient            : %s" % opt.at_tc
+    print "--------------------------------------------------------------------"
+    print "Equation of time                             : %s min" % res['eqt']
+    print "Inverse relative distance factor             : %s" % res['sol_r']
+    print "Sun declination                              : %s°"  % res['sol_d']
+    print "Solar Noon                                   : %s "  % res['sol_n']
+    print "Barometric Pressure at site                  : %s kPa" % opt.at_p
+    print "Estimated Vapor Pressure at site             : %s kPa" % res['at_vp']
+    print "Estimated extraterrestrial Radiation         : %s W/m²" % res['ETR']
+    print "Estimated precipitable water in Atmosphere   : %s mm" % res['at_pw']
+    print "Clearness index for direct beam radiation    : %s" % res['CIDBR']
+    print "Transmissivity index for diffuse radiation   : %s" % res['TIDR']
+    print "--------------------------------------------------------------"
+    print "Model estimated Shortwave Radiation (RSO)    : \033[1;33m%3.1f W/m²\033[0m" % res['RSO']
+    print "Optimum Elevation of PV-Panel                : \033[1;37m%02.1f°\033[0m" % res['sol_z']
+    print "Model estimated Max. PV-Power Output         : \033[1;32m%3.1f W\033[0m \033[1;37m@ %d%% Mod Eff\033[0m" % (res['pv_max'], opt.pv_e)
+    print "Model estimated PV-Module temp conv. loss    : -\033[1;31m%2.1f W / %2.1f%%\033[0m" % (res['pv_lp'] , res['pv_l'] )
+    print "Model estimated PV-Module aging loss         : -\033[1;31m%03.1f W\033[0m" % res['pv_la']
+    print "--------------------------------------------------------------"
+    print "Model estimated Real PV-Power Output         : \033[1;32m%3.1f W\033[0m" % res['pv_out']
+
+def options(arg):
 
     # Set Solar Constant - See docs/solar-constant.pdf for more info. ##########
     # Default value of 1361.0 should IMHO serve as a good average point
@@ -133,26 +156,27 @@ def setup(arg):
 def main():
 
     arg             = argparse.ArgumentParser()
-    setup           (arg)
-    options         = arg.parse_args()
+    options         (arg)
+    opt             = arg.parse_args()
 
-    odate           = options.date.split("-")
-    year            = int(odate[0])
-    month           = int(odate[1])
-    day             = int(odate[2])
+    opt.date        = opt.date.split("-")
+    opt.year        = int(opt.date[0])
+    opt.month       = int(opt.date[1])
+    opt.day         = int(opt.date[2])
 
-    otime           = options.time.split(":")
-    hour            = int(otime[0])
-    min             = int(otime[1])
-    sec             = int(otime[2])
-    ToD             = hour
+    opt.time        = opt.time.split(":")
+    opt.hour        = int(opt.time[0])
+    opt.min         = int(opt.time[1])
+    opt.sec         = int(opt.time[2])
 
     dst_off         = 0
-    tz_off_deg      = 0 + options.lon
+    tz_off_deg      = 0 + opt.lon
+
+    res             = {}
 
     # get Julian Day (Day of Year)
 
-    if              calendar.isleap(year):
+    if              calendar.isleap(opt.year):
 
         # Leap year (366 days)
         lMonth      = [0,31,60,91,121,152,182,213,244,274,305,335,366]
@@ -162,173 +186,122 @@ def main():
         # Normal year (365 days)
         lMonth      = [0,31,59,90,120,151,181,212,243,273,304,334,365]
 
-    DoY             = lMonth[month-1] + day
-
-    print "--------------------------------------------------------------"
-
-    print "%d.%d.%d | %d | %f | " % (day, month, year, DoY, ToD )
-
-    print "--------------------------------------------------------------"
-
-    print "Solar Constant                               : %s" % options.sc
-    print "Atmospheric turbidity coefficient            : %s" % options.at_tc
-
-    print "--------------------------------------------------------------"
-
-
-    # Compute inverse relative distance factor (Distance between Earth and Sun)
-
-    sun_rel_dist_f  = 1.0/(1.0-9.464e-4*math.sin(DoY)-                      \
-                    + 0.01671*math.cos(DoY)-                                \
-                    + 1.489e-4*math.cos(2.0*DoY)-2.917e-5*math.sin(3.0*DoY)-\
-                    + 3.438e-4*math.cos(4.0*DoY))**2
-
-    print "Inverse relative distance factor             : %s" % sun_rel_dist_f
-
-    # Compute solar declination ################################################
-
-    sun_decl        = (math.asin(0.39785*(math.sin(((278.97+(0.9856*DoY))   \
-                    + (1.9165*(math.sin((356.6+(0.9856*DoY))                \
-                    * (math.pi/180)))))*(math.pi/180))))*180)               \
-                    / math.pi
-
-
-    print "Sun declination                              : %s°" % sun_decl
-
+    res['DoY']      = lMonth[opt.month-1] + opt.day
+    res['ToD']      = opt.hour
 
     # Solve equation of time ###################################################
     # (More info on http://www.srrb.noaa.gov/highlights/sunrise/azel.html)
 
-    eqt             = (((5.0323-(430.847*math.cos((((2*math.pi)*DoY)/366)+4.8718)))\
-                    + (12.5024*(math.cos(2*((((2*math.pi)*DoY)/366)+4.8718))))\
-                    + (18.25*(math.cos(3*((((2*math.pi)*DoY)/366)+4.8718))))\
-                    - (100.976*(math.sin((((2*math.pi)*DoY)/366)+4.8718))))\
-                    + (595.275*(math.sin(2*((((2*math.pi)*DoY)/366)+4.8718))))\
-                    + (3.6858*(math.sin(3*((((2*math.pi)*DoY)/366)+4.871))))\
-                    - (12.47*(math.sin(4*((((2*math.pi)*DoY)/366)+4.8718)))))\
+    res['eqt']      = (((5.0323-(430.847*math.cos((((2*math.pi)*res['DoY'])/366)+4.8718)))\
+                    + (12.5024*(math.cos(2*((((2*math.pi)*res['DoY'])/366)+4.8718))))\
+                    + (18.25*(math.cos(3*((((2*math.pi)*res['DoY'])/366)+4.8718))))\
+                    - (100.976*(math.sin((((2*math.pi)*res['DoY'])/366)+4.8718))))\
+                    + (595.275*(math.sin(2*((((2*math.pi)*res['DoY'])/366)+4.8718))))\
+                    + (3.6858*(math.sin(3*((((2*math.pi)*res['DoY'])/366)+4.871))))\
+                    - (12.47*(math.sin(4*((((2*math.pi)*res['DoY'])/366)+4.8718)))))\
                     / 60
 
-    print "Equation of time                             : %s min" % eqt
+    # Compute inverse relative distance factor (Distance between Earth and Sun)
+
+    res['sol_r']    = 1.0 / (1.0 - 9.464e-4 * math.sin(res['DoY'])                      \
+                    - 0.01671  * math.cos(res['DoY'])                                \
+                    - 1.489e-4 * math.cos(2.0 * res['DoY'])                           \
+                    - 2.917e-5 * math.sin(3.0 * res['DoY'])                           \
+                    - 3.438e-4 * math.cos(4.0 * res['DoY'])) ** 2
 
 
-    # time of solar noon ###################################################
+    # Compute solar declination ################################################
 
-    sol_noon        = ((12+dst_off)-(eqt/60))-((tz_off_deg-options.lon)/15)
+    res['sol_d']    = (math.asin(0.39785 * (math.sin(((278.97 \
+                    + (0.9856 * res['DoY'])) + (1.9165 \
+                    * (math.sin((356.6 + (0.9856 * res['DoY']))                \
+                    * (math.pi / 180))))) * (math.pi / 180)))) * 180) / math.pi
 
-    print "Solar Noon                                   : %s " % sol_noon
 
 
-    # solar zenith angle in DEG ############################################
+    # Compute time of solar noon ###########################################
 
-    sol_zen         = math.acos(((math.sin(options.lat*(math.pi/180)))        \
-                    * (math.sin(sun_decl*(math.pi/180))))                       \
-                    + (((math.cos(options.lat*((math.pi/180))))               \
-                    * (math.cos(sun_decl*(math.pi/180))))                       \
-                    * (math.cos((ToD-sol_noon)*(math.pi/12)))))                 \
-                    * (180/math.pi)
+    res['sol_n']    = ((12 + dst_off) - (res['eqt'] / 60))\
+                    - ((tz_off_deg - opt.lon) / 15)
+
+    # Compute solar zenith angle in DEG ####################################
+
+    res['sol_z']    = math.acos(((math.sin(opt.lat * (math.pi / 180)))        \
+                    * (math.sin(res['sol_d'] * (math.pi / 180))))                       \
+                    + (((math.cos(opt.lat * ((math.pi / 180))))               \
+                    * (math.cos(res['sol_d'] * (math.pi / 180))))                       \
+                    * (math.cos((res['ToD'] - res['sol_n']) \
+                    * (math.pi /12))))) * (180/math.pi)
 
     # in extreme latitude, values over 90 may occurs.
-    if sol_zen > 90:
-        sol_zen     = 90
+    if res['sol_z'] > 90:
+        res['sol_z']= 90
 
-    print "Solar Zenith Angle                           : %s° " % sol_zen
-
-
-    # Barometric pressure of the measurement site
+    # Barometric pressure at site ###########################################
     # (this should be replaced by the real measured value) in kPa
 
-    atm_press       = 101.325                                                   \
-                    * math.pow(((288-(0.0065*(options.alt-0)))/288)           \
-                    , (9.80665/(0.0065*287)))
+    if opt.at_p:
+        # Real value given, convert hPa to kPa
+        opt.at_p    = opt.at_p / 10
+    else:
+        # Estimate Pressure from height
+        opt.at_p    = math.pow(((288 - (0.0065 * (opt.alt - 0))) / 288),\
+                      (9.80665 / (0.0065 * 287))) * 101.325
 
-    atm_press       = 100.5
+    # Estimate air vapor pressure in kPa ###################################
 
-    print "Estimated Barometric Pressure at site        : %s kPa" % atm_press
-
-
-    # Estimated air vapor pressure in kPa ###################################
-
-    atm_vapor_press = (0.61121*math.exp((17.502*options.at_t)                   \
-                    / (240.97+options.at_t)))                                   \
-                    * (options.at_h/100)
-
-    print "Estimated Vapor Pressure at site             : %s kPa" % atm_vapor_press
-
+    res['at_vp']    = (0.61121 * math.exp((17.502 * opt.at_t)                   \
+                    / (240.97 + opt.at_t)))                                   \
+                    * (opt.at_h / 100)
 
     # Extraterrestrial radiation in W/m2 ###################################
 
-    extra_terr_rad  = (options.sc*sun_rel_dist_f)                            \
-                    * (math.cos(sol_zen*(math.pi/180)))
-
-    print "Estimated Extraterrestrial radiation         : %s W/m²" % extra_terr_rad
-
+    res['ETR']      = (opt.sc * res['sol_r'])                            \
+                    * (math.cos(res['sol_z'] * (math.pi / 180)))
 
     # Precipitable water in the atmosphere in mm ###########################
 
-    atm_prec_h2o    = ((0.14*atm_vapor_press)*atm_press)+2.1
-
-    print "Estimated precipitable water in Atmosphere   : %s mm" % atm_prec_h2o
-
+    res['at_pw']    = ((0.14 * res['at_vp']) * opt.at_p) + 2.1
 
     # Clearness index for direct beam radiation [unitless] #################
 
-    clr_idx_beam_rad= 0.98*(math.exp(((-0.00146*atm_press)                  \
-                    / (options.at_tc*(math.sin((90-sol_zen)*(math.pi/180)))))      \
-                    - (0.075*(math.pow((atm_prec_h2o                        \
-                    / (math.sin((90-sol_zen)*(math.pi/180)))),0.4)))))
-
-    print "Clearness index for direct beam radiation    : %s" % clr_idx_beam_rad
-
+    res['CIDBR']    = 0.98 * (math.exp(((-0.00146 * opt.at_p)                  \
+                    / (opt.at_tc * (math.sin((90 - res['sol_z'])                   \
+                    * (math.pi / 180))))) - (0.075 * (math.pow((res['at_pw']      \
+                    / (math.sin((90 - res['sol_z']) * (math.pi / 180)))),0.4)))))
 
     # Transmissivity index for diffuse radiation [unitless] ####################
 
-    if                              (clr_idx_beam_rad > 0.15):
-        trns_idx_diff_rad=          0.35-(0.36*clr_idx_beam_rad)
+    if (res['CIDBR'] > 0.15):
+
+        res['TIDR'] = 0.35 - (0.36 * res['CIDBR'])
+
     else:
-        trns_idx_diff_rad=          0.18+(0.82*clr_idx_beam_rad)
 
-    print "Transmissivity index for diffuse radiation   : %s" % trns_idx_diff_rad
-
+        res['TIDR'] = 0.18 + (0.82 * res['CIDBR'])
 
     # Model Estimated Shortwave Radiation (W/m2) ###############################
 
-    est_sol_rad     =               (clr_idx_beam_rad + trns_idx_diff_rad)      \
-                    *               extra_terr_rad
-
-    print "--------------------------------------------------------------"
-    print "Model Estimated Shortwave Radiation (RSO)    : \033[1;33m%3.1f W/m²\033[0m" % est_sol_rad
-
-    # Optimal PV Module Angle to Sun
-
-    print "Optimum elevation of PV-Panel                : \033[1;37m%02.1f°\033[0m" % sol_zen
-
+    res['RSO']      = (res['CIDBR'] + res['TIDR']) * res['ETR']
 
     # Estimate Theoretical Max. Power Output (Panel at nominal Efficiency) #####
 
-    est_p_out       =               (est_sol_rad * options.pv_a)                \
-                    /               100 * options.pv_e
-
-    print "Model Estimated Max. PV-Power Output         : \033[1;32m%3.1f W\033[0m \033[1;37m@ %d%% Mod Eff\033[0m" % (est_p_out, options.pv_e)
+    res['pv_max']   = (res['RSO'] * opt.pv_a) / 100 * opt.pv_e
 
     # Estimate conversion loss due to module temperature ###################
 
-    pv_p_loss       =               (options.pv_t-25 ) * options.pv_tc
-    pv_p_loss_p     =               (est_p_out/100) * pv_p_loss
-
-    print "Model estimated PV-Module temp conv. loss    : -\033[1;31m%2.1f W / %2.1f%%\033[0m" % (pv_p_loss_p, pv_p_loss)
+    res['pv_l']     = (opt.pv_t-25 ) * opt.pv_tc
+    res['pv_lp']    = (res['pv_max'] / 100) * res['pv_l']
 
     # Estimate conversion loss due to module age
 
-    est_pv_age_loss =               est_p_out - (est_p_out * options.pv_ac)
+    res['pv_la']    = res['pv_max'] - (res['pv_max'] * opt.pv_ac)
 
-    print "Model estimated PV-Module aging loss         : -\033[1;31m%03.1f W\033[0m" % est_pv_age_loss
+    # Estimate final System Power output
 
-    # Estime final System Power output
+    res['pv_out']   = res['pv_max'] - res['pv_la'] - res['pv_lp']
 
-    est_real_p_out  =               est_p_out - est_pv_age_loss-pv_p_loss_p
-
-    print "--------------------------------------------------------------"
-    print "Model Estimated Real PV-Power Output         : \033[1;32m%3.1f W\033[0m" % est_real_p_out
+    output          (opt, res)
 
     return 0
 
