56cd40617f016083b282ec579a409ba183e39eda
chrono <chrono@open-resource.org>
2014-11-15 12:56:24 +0100

    Unit fix

--- ucsspm.py
+++ ucsspm.py
@@ -172,7 +172,7 @@ def output(opt,res):
         print "Solar Noon                                   : %s "  % res['sol_n']
         print "Barometric Pressure at site                  : %s kPa" % opt.at_p
         print "Estimated Vapor Pressure at site             : %s kPa" % res['at_vp']
-        print "Estimated extraterrestrial Radiation         : %s kW/m²" % res['ETR']
+        print "Estimated extraterrestrial Radiation         : %s W/m²" % res['ETR']
         print "Estimated precipitable water in Atmosphere   : %s mm" % res['at_pw']
         print "Clearness index for direct beam radiation    : %s" % res['CIDBR']
         print "Transmissivity index for diffuse radiation   : %s" % res['TIDR']
@@ -180,7 +180,10 @@ def output(opt,res):
         print "Model estimated Global solar radiation (Rs)  : \033[1;33m%3.1f W/m²\033[0m" % res['RSO']
         print "Optimum Elevation of PV-Panel                : \033[1;37m%02.1f°\033[0m" % res['sol_z']
         print "Model estimated Max. PV-Power Output         : \033[1;32m%3.1f W\033[0m \033[1;37m@ %d%% Mod Eff\033[0m" % (res['pv_max'], opt.pv_e)
-        print "Model estimated PV-Module temp conv. loss    : -\033[1;31m%2.1f W / %2.1f%%\033[0m" % (res['pv_lp'] , res['pv_l'] )
+        if res['pv_lp'] >= 0:
+            print "Model estimated PV-Module temp conv. loss    : -\033[1;31m%2.1f W / %2.1f%%\033[0m" % (res['pv_lp'] , res['pv_l'] )
+        else:
+            print "Model estimated PV-Module temp conv. gain    : +\033[1;32m%2.1f W / %2.1f%%\033[0m" % (res['pv_lp']*-1 , res['pv_l']*-1 )
         print "Model estimated PV-Module aging loss         : -\033[1;31m%03.1f W\033[0m" % res['pv_la']
         print "--------------------------------------------------------------"
         print "Model estimated Real PV-Power Output         : \033[1;32m%3.1f W\033[0m" % res['pv_out']
